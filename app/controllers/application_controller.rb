58afcead1b7b7e54a2683e591b073e4cdfbfc092
Dani Lyn <dlyn1110@gmail.com>
2019-09-06 14:24:03 +0000

    add migrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
+require './config/environment'
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
+
+  use Rack::Flash
+
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views' enable :sessions
-    set :session_secret, 'password_security'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get '/' do
-    erb :views
+   erb :index
   end
 end
