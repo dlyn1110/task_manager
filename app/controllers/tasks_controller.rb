73f85daed86dd538c2be83d150278e716888f249
Dani Lyn <dlyn1110@gmail.com>
2019-09-12 20:06:05 +0000

    removed some routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,78 +1,7 @@
 class TasksController < ApplicationController
 
   get '/tasks'do
-    if logged_in?
-      @tasks = current_user.tasks
+  
       erb :'tasks/index'
-    else
-      redirect to '/signup'
    end
   end
-
-  get '/tasks/new' do
-    if logged_in?
-      erb :'tasks/create'
-    else
-      redirect to '/login'
-    end
-  end
-
-  get '/tasks/:id' do
-    if logged_in?
-      @task = Task.find_by(:id => params[:id])
-      erb :"tasks/show"
-    else
-      redirect '/login'
-    end
-  end
-
-
-  post '/tasks' do
-    if logged_in?
-      @task = current_user.tasks.create(:name => params[:name])
-      flash[:notice] = "Your Task Has Been Succesfully Created"
-      redirect "/tasks"
-    else
-      erb '/tasks/create'
-    end
-  end
-
-  get '/tasks/:id/edit' do
-    if logged_in?
-      @task = Task.find_by(:id => params[:id])
-      if current_user.id == @task.user_id
-      erb :"tasks/edit"
-    else
-      flash[:notice] = "You are not authorized to edit this task."
-      redirect to '/login'
-     end
-    end
-  end
-
-  patch '/tasks/:id' do
-    if logged_in?
-      @task = Task.find_by(:id => params[:id])
-      @task.name = params[:name]
-      if @task.save
-        flash[:notice] = "Your Task Has Been Succesfully Updated"
-        redirect to '/tasks'
-      else
-        redirect to "/tasks/#{@task.id}/edit"
-      end
-    end
-  end
-
-  delete '/tasks/:id/delete' do
-    if logged_in?
-      @task = Task.find_by(:id => params[:id])
-      if current_user.id == @task.user_id
-      @task.delete
-      flash[:notice] = "Task successfully removed!"
-      redirect to '/tasks'
-      else
-      redirect to '/login'
-      end
-    end
-  end
-
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,43 +5,8 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    @user = User.new(params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to "/tasks"
-       #validations passed
-    else
+
       erb :'users/signup'
         #validations failed
     end
   end
-
-  get '/login' do
-    if logged_in?
-      redirect '/tasks'
-    else
-      erb :'users/login'
-    end
-  end
-
-  post '/login' do
-    @user = User.find_by(:email => params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/tasks'
-    else
-      flash[:notice] = "Please sign up to Planner before login."
-      redirect '/signup'
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect '/login'
-    else
-      redirect '/'
-    end
-  end
-
-end
